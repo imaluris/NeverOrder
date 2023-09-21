ef44f16a268585c33d5a549f169d1c06563374c9
iMaLuRiS <andrea.93@hotmail.it>
2023-09-21 09:57:50 +0200

    animation rect and table fixed

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
   styleUrls: ['./user-dashboard.component.scss']
 })
-export class UserDashboardComponent {
+export class UserDashboardComponent{
   ntable: number[] = [1,2,3,4,5,6,7,8,9,10,11,12];
-
+  
 }
